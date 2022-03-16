e08780480c7c55fc053ccd2726c8024cdd8a6df9
sandesh katakam <sandeshkatakam@gmail.com>
2022-03-16 21:42:15 +0530

    Added sample to tests to view the results

--- src/My-Julia-ML-Package.jl
+++ src/My-Julia-ML-Package.jl
@@ -2,4 +2,10 @@ module My-Julia-ML-Package
 
 # Write your package code here.
 println("hello, my first line for my Julia ML package hope this goes well")
+
+function sum_of_three(x,y,z)
+    return x+y+z
+end
+
+sum_of_three(3,4,5)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,7 @@ using Test
 
 @testset "My-Julia-ML-Package.jl" begin
     # Write your tests here.
+    @test sum_of_three(3,4,5) == 12
+    @test sum_of_three(5,5,5) == 15
+    @test sum_of_three(6,6,6) == 18
 end
